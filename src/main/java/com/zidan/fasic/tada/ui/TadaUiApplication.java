5e2292c9cab66343ff512a1e8336fe8b83c9289e
nemes.vik@gmail.com <nemes.vik@gmail.com>
2018-05-29 22:19:02 +0200

    Application stub

---
+++ src/main/java/com/zidan/fasic/tada/ui/TadaUiApplication.java
@@ -0,0 +1,39 @@
+package com.zidan.fasic.tada.ui;
+
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+
+@EnableAutoConfiguration
+@ComponentScan({"com.zidan.fasic.tada.ui.bean"})
+@SpringBootApplication
+public class TadaUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadaUiApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		return new ServletRegistrationBean(servlet, "*.jsf");
+	}
+
+	@Bean
+	public FilterRegistrationBean rewriteFilter() {
+		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+		rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+				DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
+	}
+}
