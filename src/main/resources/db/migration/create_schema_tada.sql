0c4ae4642316678719a05df763514c1e491b5d49
Lopfest <lopfest@gmail.com>
2018-05-30 11:36:55 +0200

    updated create_schema

--- src/main/resources/db/migration/create_schema_tada.sql
+++ src/main/resources/db/migration/create_schema_tada.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `tada`
+CREATE DATABASE IF NOT EXISTS `tada`;
 USE `tada`;
 
 DROP TABLE IF EXISTS `dictionary`;
@@ -6,12 +6,12 @@ DROP TABLE IF EXISTS `dictionary`;
 CREATE TABLE `dictionary` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `abbreviation` varchar(200) NOT NULL,
-  `explanation` text NOT NULL,
+  `explanation` text,
   `notes` text,
-  `status` varchar(45) NOT NULL,
+  `status` enum('confirmed', 'suggested', 'requested'),
   `inserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `hitcount` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-)
+);
 
 
