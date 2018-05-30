7ccada54e107aa3a23f95a853e0502bd388907f5
nemes.vik@gmail.com <nemes.vik@gmail.com>
2018-05-30 11:43:00 +0200

    dictionary repo

---
+++ src/main/java/com/zidan/fasic/tada/ui/db/repository/DictionaryRepository.java
@@ -0,0 +1,10 @@
+package com.zidan.fasic.tada.ui.db.repository;
+
+import com.zidan.fasic.tada.ui.db.entity.DictionaryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by viktor on 30.5.2018.
+ */
+public interface DictionaryRepository extends JpaRepository<DictionaryEntity, Long> {
+}
